3459979e6ce93f0a3f3e37b8c8f3cf66154e6f74
Vojtěch Červený <cervik49@gmail.com>
2020-11-10 16:36:35 +0100

    adding tasks

--- test/ukoly/task3.js
+++ test/ukoly/task3.js
@@ -1,3 +1,15 @@
+/* ZADANI:
+  Ověř, že tlačíko Přidej kočku přidá kartu s kočkou.
+  Ověř, že tlačíko Přidej kočku dokáže přidat vícero karet s kočkou.
+  Ověř, že tlačíko Odeber kočku odebre jednu kartu s kočkou.
+  Ověř, že tlačíko Apokalypsa! smaže všechny karty s kočkou.
+  Ověř, že počítadlo koček reaguje na přidání kočky, smazání kočky, smazání všech koček
+  Ověř, že lze přidat 20 karet s kočkou.
+  Zajisti, aby Tvé testy byly atomické = aby byly na sobě nezávislé, daly se spouštět jednotlivě.
+  Ověř, že tlačítka na odebrání jsou aktivní jen, když je zobrazena aspoň jedna karta kočky
+  Ověř, že tlačítka na odebrání jsou deaktivována po smazání poslední karty kočky.
+*/
+
 describe('what to test', () => { 
   before(()=>{
     browser.url('https://automation.cervik.repl.co/adding.html')
